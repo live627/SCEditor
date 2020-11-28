a3164b4e7f2032d827d93cd6768e9a67349f05dc
John Rayes <live627@gmail.com>
2020-11-28 15:45:57 -0700

    chore: Start testing with Ava (All tests must be reviewed and re-added)

--- tests/unit/lib/escape.js
+++ tests/unit/lib/escape.js
@@ -1,72 +1,81 @@
-import * as escape from 'src/lib/escape.js';
+import test from 'ava';
+import * as escape from '../../../src/lib/escape.js';
 
-QUnit.module('lib/escape');
-
-
-QUnit.test('regex()', function (assert) {
-	assert.equal(
+test('regex()', function (assert) {
+	assert.is(
 		escape.regex('- \\ ^ / $ * + ? . ( ) | { } [ ] | ! :'),
 		'\\- \\\\ \\^ \\/ \\$ \\* \\+ \\? \\. \\( \\) \\| ' +
 			'\\{ \\} \\[ \\] \\| \\! \\:'
 	);
 });
 
-QUnit.test('regex() - Emoticons', function (assert) {
-	assert.equal(
+test('regex() - Emoticons', function (assert) {
+	assert.is(
 		escape.regex('^^ >.< =)'),
 		'\\^\\^ >\\.< \\=\\)'
 	);
 });
 
 
-QUnit.test('entities()', function (assert) {
-	assert.strictEqual(escape.entities(null), null);
-	assert.strictEqual(escape.entities(''), '');
+test('entities()', function (assert) {
+	assert.true(escape.entities(null) === null);
+	assert.true(escape.entities('') === '');
 
-	assert.equal(
+	assert.is(
 		escape.entities('& < > " \'    `'),
 		'&amp; &lt; &gt; &#34; &#39;&nbsp; &nbsp; &#96;'
 	);
 
-	assert.equal(
+	assert.is(
 		escape.entities('& < > " \'    `', false),
 		'&amp; &lt; &gt; " \'&nbsp; &nbsp; `'
 	);
 });
 
-QUnit.test('entities() - XSS', function (assert) {
-	assert.equal(
+test('entities() - XSS', function (assert) {
+	assert.is(
 		escape.entities('<script>alert("XSS");</script>'),
 		'&lt;script&gt;alert(&#34;XSS&#34;);&lt;/script&gt;'
 	);
 });
 
-QUnit.test('entities() - IE XSS', function (assert) {
-	assert.equal(
+test('entities() - IE XSS', function (assert) {
+	assert.is(
 		escape.entities('<img src="x" alt="``onerror=alert(1)" />'),
 		'&lt;img src=&#34;x&#34; alt=&#34;&#96;&#96;onerror=alert(1)&#34; /&gt;'
 	);
 });
 
+  const url = "http://dummy.com/";
+global.window = {location: new URL(url)};
 
-QUnit.test('uriScheme() - No schmes', function (assert) {
+//~ Object.defineProperty(window, 'location', {
+  //~ value: {
+    //~ href: url,
+    //~ protocol: 'http',
+    //~ host: 'dummy.com',
+    //~ pathname: '',
+  //~ }
+//~ });
+  Object.defineProperty(window, "location", {
+    value: new URL(url)
+  } );
+
+test('uriScheme() - No schmes', function (assert) {
 	var urls = [
 		'',
 		'/test.html',
 		'//localhost/test.html',
 		'www.example.com/test?id=123'
 	];
-
-	assert.expect(urls.length);
-
 	for (var i = 0; i < urls.length; i++) {
 		var url = urls[i];
 
-		assert.equal(escape.uriScheme(url), url);
+		assert.is(escape.uriScheme(url), url);
 	}
 });
 
-QUnit.test('uriScheme() - Valid schmes', function (assert) {
+test('uriScheme() - Valid schmes', function (assert) {
 	var urls = [
 		'http://localhost',
 		'https://example.com/test.html',
@@ -84,38 +93,28 @@ QUnit.test('uriScheme() - Valid schmes', function (assert) {
 		'data:image/jpg;test',
 		'data:image/bmp;test'
 	];
-
-	assert.expect(urls.length);
-
 	for (var i = 0; i < urls.length; i++) {
 		var url = urls[i];
-
-		assert.equal(escape.uriScheme(url), url);
+		assert.is(escape.uriScheme(url), url);
 	}
 });
 
-QUnit.test('uriScheme() - Invalid schmes', function (assert) {
-	var path = location.pathname.split('/');
+test('uriScheme() - Invalid schmes', function (assert) {
+	var path = window.location.pathname.split('/');
 	path.pop();
 
-	var baseUrl = location.protocol + '//' +
-		location.host +
+	var baseUrl = window.location.protocol + '//' +
+		window.location.host +
 		path.join('/') + '/';
 
-	/*jshint scripturl:true*/
 	var urls = [
-		// eslint-disable-next-line no-script-url
 		'javascript:alert("XSS");',
 		'jav	ascript:alert(\'XSS\');',
 		'vbscript:msgbox("XSS")',
 		'data:application/javascript;alert("xss")'
 	];
-
-	assert.expect(urls.length);
-
 	for (var i = 0; i < urls.length; i++) {
 		var url = urls[i];
-
-		assert.equal(escape.uriScheme(url), baseUrl + url);
+		assert.is(escape.uriScheme(url), baseUrl + url);
 	}
 });
