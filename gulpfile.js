a8b7b00e150fda0fb57926790e9b339f62fe73ee
John Rayes <live627@gmail.com>
2020-11-27 21:09:17 -0700

    Move all build tasks from Grunt to Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,96 @@
-'use strict';
+if (!String.format) {
+	String.format = function (format) {
+		var args = Array.prototype.slice.call(arguments, 1);
+		return format.replace(/{(\d+)}/g, function (match, number) {
+			return typeof args[number] !== 'undefined'
+				? args[number]
+				: match
+			;
+		});
+	};
+}
+import gulp from 'gulp';
+import rename from 'gulp-rename';
+import uglify from 'gulp-uglify';
+import sass   from 'gulp-sass';
+import  cssmin   from 'gulp-cssmin';
+import  spritesmith   from 'gulp.spritesmith';
+import {rollup} from 'rollup';
+import  zip   from 'gulp-zip';
 
-let gulp       = require('gulp');
-var spritesmith = require('gulp.spritesmith');
+let
+	javascript = () => {
+		return rollup.rollup({
+			input: './src/sceditor.js'
+		}).then(bundle => {
+			return bundle.write({
+				file: './dist/sceditor.js',
+				format: 'iife'
+			});
+		});
+	},
+	m = () => {
+		return gulp.src('./dist/sceditor.js')
+			.pipe(uglify())
+			.pipe(rename({ suffix: '.min' }))
+			.pipe(gulp.dest('./dist'));
+	},
+	sprite = () => {
+		var spriteData = gulp.src('src/themes/icons/src/famfamfam/*.png')
+			.pipe(spritesmith({
+				imgName: 'famfamfam.png',
+				cssName: 'famfamfam.sass',
+				cssTemplate: function (data) {
+					var spriteObj = [
+						`div.sceditor-grip, .sceditor-button div,
+	background-image: url("famfamfam.png")
+	background-repeat: no-repeat
+	width: 16px
+	height: 16px`
+					];
+					data.sprites.forEach(function (sprite) {
+						spriteObj.push(
+							String.format(`.sceditor-button-{0} div
+	background-position: {1} {2}'`,
+							sprite.name,
+							sprite.px.offset_x, sprite.px.offset_y));
+					});
+					return spriteObj.join('\n');
+				}
+			}));
+		return spriteData.pipe(gulp.dest('src/themes/icons/'));
+	},
+	copy = () => {
+		return gulp.src('src/themes/icons/famfamfam.png')
+			.pipe(gulp.dest('dist'));
+	},
+	css = () => {
+		return gulp.src('src/themes/*.scss')
+			.pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+			.pipe(gulp.dest('dist'))
+			.pipe(cssmin())
+			.pipe(rename({suffix: '.min'}))
+			.pipe(gulp.dest('dist'));
+	},
+	z = () => {
+		return gulp.src(['dist/*', '!dist/sceditor.zip'])
+			.pipe(zip('sceditor.zip'))
+			.pipe(gulp.dest('dist'));
+	},
+	coverage = () => {
+		return gulp.src(['src/**/*.js'])
+		// Covering files
+			.pipe(istanbul())
+		// Force `require` to return covered files
+			.pipe(istanbul.hookRequire());
+	};
 
- 
-gulp.task('sprite', function () {
-  var spriteData = gulp.src('src/themes/icons/src/famfamfam/*.png').pipe(spritesmith({
-    imgName: 'famfamfam.png',
-    cssName: 'famfamfam.sass',
-  cssTemplate: function (data) {
-    // Convert sprites from an array into an object
-    var spriteObj = [
-'div.sceditor-grip, .sceditor-button div',
-'	background-image: url("famfamfam.png")',
-'	background-repeat: no-repeat',
-'	width: 16px',
-'	height: 16px'
-    ];
-    data.sprites.forEach(function (sprite) {
-      // Grab the name and store the sprite under it
-      var name = sprite.name;
-      spriteObj.push('.sceditor-button-' + sprite.name + ' div');
-      spriteObj.push('	background-position: ' + sprite.px.offset_x + ' ' + sprite.px.offset_y);
-    });
-    return spriteObj.join('\n');
-  }
-  }));
-  return spriteData.pipe(gulp.dest('src/themes/icons/'));
-});
+export default gulp.series(javascript, m, sprite, copy, css, z);
+export {
+	javascript,
+	uglify as m,
+	sprite,
+	css,
+	zip  as z,
+	test
+};
