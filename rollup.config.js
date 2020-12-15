ff86690429818b7de9841f7948e73b2e46338da2
John Rayes <live627@gmail.com>
2020-12-15 05:20:33 -0700

    create combined bundles

--- rollup.config.js
+++ rollup.config.js
@@ -16,10 +16,26 @@ export default [
 		}
 	},
 	{
+		input: './src/sceditor.bbcode.js',
+		output: {
+			file: './dist/sceditor.bbcode.js',
+			name: 'sceditor',
+			format: 'iife'
+		}
+	},
+	{
 		input: './src/formats/xhtml.js',
 		output: {
 			file: './dist/xhtml.js',
 			name: 'xhtml',
+			format: 'iife'
+		}
+	},
+	{
+		input: './src/sceditor.xhtml.js',
+		output: {
+			file: './dist/sceditor.xhtml.js',
+			name: 'sceditor',
 			format: 'iife'
 		}
 	}
---
+++ src/sceditor.xhtml.js
@@ -0,0 +1,19 @@
+/**
+ * SCEditor
+ * http://www.sceditor.com/
+ *
+ * Copyright (C) 2017, Sam Clarke (samclarke.com)
+ *
+ * SCEditor is licensed under the MIT license:
+ *	http://www.opensource.org/licenses/mit-license.php
+ *
+ * @fileoverview SCEditor - A lightweight WYSIWYG BBCode and HTML editor
+ * @author Sam Clarke
+ */
+
+import sceditor from './sceditor.js';
+import plugin from './formats/xhtml.js';
+
+sceditor.format = plugin;
+
+export default sceditor;
