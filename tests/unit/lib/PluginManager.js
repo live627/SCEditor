ff76ffe0f6ab00cd7041d7816d8461b369d0ed76
John Rayes <live627@gmail.com>
2021-01-16 21:34:45 -0700

    drop dead code

--- tests/unit/lib/PluginManager.js
+++ tests/unit/lib/PluginManager.js
@@ -5,84 +5,15 @@ import PluginManager from '../../../src/lib/PluginManager.js';
 var fakeEditor = {};
 var fakePlugin;
 var fakePluginTwo;
-var pluginManager, sandbox;
+var pluginManager;
 
 test.beforeEach(() => {
-	//~ sandbox = sinon.sandbox.create();
 	fakePlugin    = function () {};
 	fakePluginTwo = function () {};
 
 	pluginManager = new PluginManager(fakeEditor);
 	PluginManager.plugins.fakePlugin = fakePlugin;
 	PluginManager.plugins.fakePluginTwo = fakePluginTwo;
-});
-
-//~ test.afterEach.always(() => {
-    //~ sandbox.restore();
-//~ });
-
-test.serial('call()', function (assert) {
-	var arg = {};
-	var firstSpy = sinon.spy();
-	var secondSpy = sinon.spy();
-
-	fakePlugin.prototype.signalTest = firstSpy;
-	fakePluginTwo.prototype.signalTest = secondSpy;
-
-	pluginManager.register('fakePlugin');
-	pluginManager.register('fakePluginTwo');
-
-	pluginManager.call('test', arg);
-
-	assert.true(firstSpy.calledOnce);
-	assert.true(firstSpy.calledOn(fakeEditor));
-	assert.true(firstSpy.calledWithExactly(arg));
-	assert.true(secondSpy.calledOnce);
-	assert.true(secondSpy.calledOn(fakeEditor));
-	assert.true(secondSpy.calledWithExactly(arg));
-
-	assert.true(firstSpy.calledBefore(secondSpy));
-});
-
-test.serial('callOnlyFirst()', function (assert) {
-	var arg = {};
-
-	var stub = sinon.stub();
-	stub.returns(1);
-
-	fakePlugin.prototype.signalTest = stub;
-	fakePluginTwo.prototype.signalTest = sinon.spy();
-
-	pluginManager.register('fakePlugin');
-	pluginManager.register('fakePluginTwo');
-
-	assert.truthy(pluginManager.callOnlyFirst('test', arg));
-
-	assert.true(fakePlugin.prototype.signalTest.calledOnce);
-	assert.true(fakePlugin.prototype.signalTest.calledOn(fakeEditor));
-	assert.true(fakePlugin.prototype.signalTest.calledWithExactly(arg));
-	assert.false(fakePluginTwo.prototype.signalTest.called);
-});
-
-test.serial('hasHandler()', function (assert) {
-	fakePlugin.prototype.signalTest = sinon.spy();
-	fakePluginTwo.prototype.signalTest = sinon.spy();
-	fakePluginTwo.prototype.signalTestTwo = sinon.spy();
-
-	pluginManager.register('fakePlugin');
-	pluginManager.register('fakePluginTwo');
-
-	assert.true(pluginManager.hasHandler('test'));
-	assert.true(pluginManager.hasHandler('testTwo'));
-});
-
-test.serial('hasHandler() - No handler', function (assert) {
-	fakePlugin.prototype.signalTest = sinon.spy();
-
-	pluginManager.register('fakePlugin');
-
-	assert.false(pluginManager.hasHandler('teSt'));
-	assert.false(pluginManager.hasHandler('testTwo'));
 });
 
 test.serial('exists()', function (assert) {
